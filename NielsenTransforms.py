1b3f9c98eb4f62fc02fde1bfc134cacc2352e1ef
pikorua <paul-cahn@web.de>
2024-03-25 11:12:01 +0100

    Extending alphabet

--- NielsenTransforms.py
+++ NielsenTransforms.py
@@ -5,7 +5,7 @@ import networkx as nx
 from pyvis.network import Network
 
 variables = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-             'V', 'W','X', 'Y', 'Z']
+             'V', 'W','X', 'Y', 'Z','1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ]
 currentNodes = []
 assignments = []
 count = [1]
